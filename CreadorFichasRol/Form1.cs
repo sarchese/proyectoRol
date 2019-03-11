a30b6ea95d58ea3a3771b84a1c618bf64719ab13
deutschbremen <jose.sarchese@gmail.com>
2019-03-11 19:32:41 +0100

    tirada()
    Creado un metodo para hacer una tirada de creacion de atributos.

--- CreadorFichasRol/Form1.cs
+++ CreadorFichasRol/Form1.cs
@@ -12,15 +12,35 @@ namespace CreadorFichasRol
 {
     public partial class Form1 : Form
     {
+        int test;
         public Form1()
         {
             InitializeComponent();
+            Tirada();
         }
 
 
         private void Tirada()
         {
             int[] array = new int[4];
+            var random = new Random();
+            //genera tantos numeros como grande sea el array
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(1, 6);
+            }
+            //ordena los numeros
+            Array.Sort(array);
+            Array.Reverse(array);
+            //suma los tres primeros
+            for (int i = 0; i < array.Length-1; i++)
+            {
+                test += array[i];
+                    //MessageBox.Show(i.ToString());                
+            }
+            //muestra en pantalla
+            MessageBox.Show(test.ToString());
+
         }
 
 
