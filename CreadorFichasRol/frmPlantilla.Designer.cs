b009381162041f3c17baa0ef7ac88293e555d47a
Jose <jose.sarchese@gmail.com>
2019-03-13 17:01:23 +0100

    bonificadores y tiradas

--- CreadorFichasRol/frmPlantilla.Designer.cs
+++ CreadorFichasRol/frmPlantilla.Designer.cs
@@ -33,8 +33,8 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.txtFue = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.txtBonFue = new System.Windows.Forms.TextBox();
+            this.Tirada = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.txtInt = new System.Windows.Forms.TextBox();
             this.textBox4 = new System.Windows.Forms.TextBox();
@@ -51,191 +51,285 @@
             this.cbAttDes = new System.Windows.Forms.ComboBox();
             this.cbAttSab = new System.Windows.Forms.ComboBox();
             this.cbAttCon = new System.Windows.Forms.ComboBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Location = new System.Drawing.Point(10, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(101, 17);
+            this.label1.Size = new System.Drawing.Size(76, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Nombre del PJ";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(13, 46);
+            this.label2.Location = new System.Drawing.Point(10, 37);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(86, 17);
+            this.label2.Size = new System.Drawing.Size(65, 13);
             this.label2.TabIndex = 1;
             this.label2.Text = "Clase del PJ";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(333, 13);
+            this.label3.Location = new System.Drawing.Point(207, 7);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(81, 17);
+            this.label3.Size = new System.Drawing.Size(61, 13);
             this.label3.TabIndex = 2;
             this.label3.Text = "Transfondo";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(333, 46);
+            this.label4.Location = new System.Drawing.Point(207, 33);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(89, 17);
+            this.label4.Size = new System.Drawing.Size(67, 13);
             this.label4.TabIndex = 3;
             this.label4.Text = "Alineamiento";
             // 
             // txtFue
             // 
-            this.txtFue.Location = new System.Drawing.Point(101, 98);
+            this.txtFue.Enabled = false;
+            this.txtFue.Location = new System.Drawing.Point(55, 112);
+            this.txtFue.Margin = new System.Windows.Forms.Padding(2);
             this.txtFue.Name = "txtFue";
-            this.txtFue.Size = new System.Drawing.Size(40, 22);
+            this.txtFue.Size = new System.Drawing.Size(31, 20);
             this.txtFue.TabIndex = 5;
             // 
-            // textBox2
+            // txtBonFue
             // 
-            this.textBox2.Location = new System.Drawing.Point(147, 98);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(30, 22);
-            this.textBox2.TabIndex = 6;
+            this.txtBonFue.Enabled = false;
+            this.txtBonFue.Location = new System.Drawing.Point(89, 112);
+            this.txtBonFue.Margin = new System.Windows.Forms.Padding(2);
+            this.txtBonFue.Name = "txtBonFue";
+            this.txtBonFue.Size = new System.Drawing.Size(24, 20);
+            this.txtBonFue.TabIndex = 6;
             // 
-            // button1
+            // Tirada
             // 
-            this.button1.Location = new System.Drawing.Point(147, 314);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 7;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.Tirada.Location = new System.Drawing.Point(89, 299);
+            this.Tirada.Margin = new System.Windows.Forms.Padding(2);
+            this.Tirada.Name = "Tirada";
+            this.Tirada.Size = new System.Drawing.Size(97, 26);
+            this.Tirada.TabIndex = 7;
+            this.Tirada.Text = "Tirada";
+            this.Tirada.UseVisualStyleBackColor = true;
+            this.Tirada.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(147, 135);
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(89, 142);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(30, 22);
+            this.textBox1.Size = new System.Drawing.Size(24, 20);
             this.textBox1.TabIndex = 10;
             // 
             // txtInt
             // 
-            this.txtInt.Location = new System.Drawing.Point(101, 137);
+            this.txtInt.Enabled = false;
+            this.txtInt.Location = new System.Drawing.Point(55, 143);
+            this.txtInt.Margin = new System.Windows.Forms.Padding(2);
             this.txtInt.Name = "txtInt";
-            this.txtInt.Size = new System.Drawing.Size(40, 22);
+            this.txtInt.Size = new System.Drawing.Size(31, 20);
             this.txtInt.TabIndex = 9;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(147, 173);
+            this.textBox4.Enabled = false;
+            this.textBox4.Location = new System.Drawing.Point(89, 173);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(2);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(30, 22);
+            this.textBox4.Size = new System.Drawing.Size(24, 20);
             this.textBox4.TabIndex = 13;
             // 
             // txtSab
             // 
-            this.txtSab.Location = new System.Drawing.Point(101, 173);
+            this.txtSab.Enabled = false;
+            this.txtSab.Location = new System.Drawing.Point(55, 173);
+            this.txtSab.Margin = new System.Windows.Forms.Padding(2);
             this.txtSab.Name = "txtSab";
-            this.txtSab.Size = new System.Drawing.Size(40, 22);
+            this.txtSab.Size = new System.Drawing.Size(31, 20);
             this.txtSab.TabIndex = 12;
             // 
             // textBox6
             // 
-            this.textBox6.Location = new System.Drawing.Point(147, 285);
+            this.textBox6.Enabled = false;
+            this.textBox6.Location = new System.Drawing.Point(90, 263);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(2);
             this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(30, 22);
+            this.textBox6.Size = new System.Drawing.Size(24, 20);
             this.textBox6.TabIndex = 22;
             // 
             // txtDes
             // 
-            this.txtDes.Location = new System.Drawing.Point(101, 285);
+            this.txtDes.Enabled = false;
+            this.txtDes.Location = new System.Drawing.Point(55, 264);
+            this.txtDes.Margin = new System.Windows.Forms.Padding(2);
             this.txtDes.Name = "txtDes";
-            this.txtDes.Size = new System.Drawing.Size(40, 22);
+            this.txtDes.Size = new System.Drawing.Size(31, 20);
             this.txtDes.TabIndex = 21;
             // 
             // textBox8
             // 
-            this.textBox8.Location = new System.Drawing.Point(147, 247);
+            this.textBox8.Enabled = false;
+            this.textBox8.Location = new System.Drawing.Point(89, 233);
+            this.textBox8.Margin = new System.Windows.Forms.Padding(2);
             this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(30, 22);
+            this.textBox8.Size = new System.Drawing.Size(24, 20);
             this.textBox8.TabIndex = 19;
             // 
             // txtCon
             // 
-            this.txtCon.Location = new System.Drawing.Point(101, 247);
+            this.txtCon.Enabled = false;
+            this.txtCon.Location = new System.Drawing.Point(55, 233);
+            this.txtCon.Margin = new System.Windows.Forms.Padding(2);
             this.txtCon.Name = "txtCon";
-            this.txtCon.Size = new System.Drawing.Size(40, 22);
+            this.txtCon.Size = new System.Drawing.Size(31, 20);
             this.txtCon.TabIndex = 18;
             // 
             // textBox10
             // 
-            this.textBox10.Location = new System.Drawing.Point(147, 210);
+            this.textBox10.Enabled = false;
+            this.textBox10.Location = new System.Drawing.Point(89, 203);
+            this.textBox10.Margin = new System.Windows.Forms.Padding(2);
             this.textBox10.Name = "textBox10";
-            this.textBox10.Size = new System.Drawing.Size(30, 22);
+            this.textBox10.Size = new System.Drawing.Size(24, 20);
             this.textBox10.TabIndex = 16;
             // 
             // txtCar
             // 
-            this.txtCar.Location = new System.Drawing.Point(101, 210);
+            this.txtCar.Enabled = false;
+            this.txtCar.Location = new System.Drawing.Point(55, 203);
+            this.txtCar.Margin = new System.Windows.Forms.Padding(2);
             this.txtCar.Name = "txtCar";
-            this.txtCar.Size = new System.Drawing.Size(40, 22);
+            this.txtCar.Size = new System.Drawing.Size(31, 20);
             this.txtCar.TabIndex = 15;
             // 
             // cbAttFue
             // 
             this.cbAttFue.FormattingEnabled = true;
-            this.cbAttFue.Location = new System.Drawing.Point(193, 98);
+            this.cbAttFue.Location = new System.Drawing.Point(124, 111);
+            this.cbAttFue.Margin = new System.Windows.Forms.Padding(2);
             this.cbAttFue.Name = "cbAttFue";
-            this.cbAttFue.Size = new System.Drawing.Size(121, 24);
+            this.cbAttFue.Size = new System.Drawing.Size(92, 21);
             this.cbAttFue.TabIndex = 23;
             // 
             // cbAttInt
             // 
             this.cbAttInt.FormattingEnabled = true;
-            this.cbAttInt.Location = new System.Drawing.Point(193, 135);
+            this.cbAttInt.Location = new System.Drawing.Point(124, 142);
+            this.cbAttInt.Margin = new System.Windows.Forms.Padding(2);
             this.cbAttInt.Name = "cbAttInt";
-            this.cbAttInt.Size = new System.Drawing.Size(121, 24);
+            this.cbAttInt.Size = new System.Drawing.Size(92, 21);
             this.cbAttInt.TabIndex = 29;
             // 
             // cbAttCar
             // 
             this.cbAttCar.FormattingEnabled = true;
-            this.cbAttCar.Location = new System.Drawing.Point(193, 173);
+            this.cbAttCar.Location = new System.Drawing.Point(124, 173);
+            this.cbAttCar.Margin = new System.Windows.Forms.Padding(2);
             this.cbAttCar.Name = "cbAttCar";
-            this.cbAttCar.Size = new System.Drawing.Size(121, 24);
+            this.cbAttCar.Size = new System.Drawing.Size(92, 21);
             this.cbAttCar.TabIndex = 30;
             // 
             // cbAttDes
             // 
             this.cbAttDes.FormattingEnabled = true;
-            this.cbAttDes.Location = new System.Drawing.Point(193, 284);
+            this.cbAttDes.Location = new System.Drawing.Point(124, 263);
+            this.cbAttDes.Margin = new System.Windows.Forms.Padding(2);
             this.cbAttDes.Name = "cbAttDes";
-            this.cbAttDes.Size = new System.Drawing.Size(121, 24);
+            this.cbAttDes.Size = new System.Drawing.Size(92, 21);
             this.cbAttDes.TabIndex = 33;
             // 
             // cbAttSab
             // 
             this.cbAttSab.FormattingEnabled = true;
-            this.cbAttSab.Location = new System.Drawing.Point(193, 246);
+            this.cbAttSab.Location = new System.Drawing.Point(124, 232);
+            this.cbAttSab.Margin = new System.Windows.Forms.Padding(2);
             this.cbAttSab.Name = "cbAttSab";
-            this.cbAttSab.Size = new System.Drawing.Size(121, 24);
+            this.cbAttSab.Size = new System.Drawing.Size(92, 21);
             this.cbAttSab.TabIndex = 32;
             // 
             // cbAttCon
             // 
             this.cbAttCon.FormattingEnabled = true;
-            this.cbAttCon.Location = new System.Drawing.Point(193, 209);
+            this.cbAttCon.Location = new System.Drawing.Point(124, 202);
+            this.cbAttCon.Margin = new System.Windows.Forms.Padding(2);
             this.cbAttCon.Name = "cbAttCon";
-            this.cbAttCon.Size = new System.Drawing.Size(121, 24);
+            this.cbAttCon.Size = new System.Drawing.Size(92, 21);
             this.cbAttCon.TabIndex = 31;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(274, 4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 34;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(274, 34);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 35;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(89, 34);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 36;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(89, 4);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 20);
+            this.textBox9.TabIndex = 37;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Menu;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 70);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(249, 269);
+            this.dataGridView1.TabIndex = 38;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(98, 87);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 13);
+            this.label5.TabIndex = 39;
+            this.label5.Text = "Atributos";
             // 
             // frmPlantilla
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.cbAttDes);
             this.Controls.Add(this.cbAttSab);
             this.Controls.Add(this.cbAttCon);
@@ -252,15 +346,18 @@
             this.Controls.Add(this.txtSab);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.txtInt);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.Tirada);
+            this.Controls.Add(this.txtBonFue);
             this.Controls.Add(this.txtFue);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "frmPlantilla";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -273,8 +370,8 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox txtFue;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtBonFue;
+        private System.Windows.Forms.Button Tirada;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox txtInt;
         private System.Windows.Forms.TextBox textBox4;
@@ -291,6 +388,12 @@
         private System.Windows.Forms.ComboBox cbAttDes;
         private System.Windows.Forms.ComboBox cbAttSab;
         private System.Windows.Forms.ComboBox cbAttCon;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- CreadorFichasRol/frmPlantilla.cs
+++ CreadorFichasRol/frmPlantilla.cs
@@ -13,7 +13,7 @@ namespace CreadorFichasRol
     public partial class frmPlantilla : Form
     {
         enum Atributo { Fue, Int, Con, Des, Sab, Car };   
-                int test;
+                int resultado;
         public frmPlantilla()
         {
             InitializeComponent();
@@ -25,40 +25,40 @@ namespace CreadorFichasRol
                 cbAttInt.Items.Add(x);
                 cbAttSab.Items.Add(x);
             } 
-        }
-     private int TiradaAtributos()
-        {
-            do
-            {
-                int[] array = new int[4];
-                var random = new Random();
-                //genera tantos numeros como grande sea el array
-                for (int i = 0; i < array.Length; i++)
-                {
-                    array[i] = random.Next(1, 6);
-                }
-                //ordena los numeros
-                Array.Sort(array);
-                Array.Reverse(array);
-                //suma los tres primeros
-                test = array[0] + array[1] + array[2];
-            } while (test < 8);            
-            return test;
-        }
+        }  
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int[] rellenarAtributos = new int[6];
-            for (int i = 0; i < rellenarAtributos.Length; i++)
-            {             
-              rellenarAtributos[i] = TiradaAtributos();                
-            }
-            txtFue.Text = TiradaAtributos().ToString();
-            txtCar.Text = rellenarAtributos[1].ToString();
-            txtCon.Text = rellenarAtributos[2].ToString();
-            txtDes.Text = rellenarAtributos[3].ToString();
-            txtSab.Text = rellenarAtributos[4].ToString();
-            txtInt.Text = rellenarAtributos[5].ToString();
-        }        
+            int[] dados = new int[4];
+            var random = new Random();
+            int[] atributos = new int[6];
+            for (int x = 0; x < atributos.Length; x++)
+            {
+                do
+                {
+                    for (int i = 0; i < dados.Length; i++)
+                    {
+                        dados[i] = random.Next(1, 7);
+                    }
+                    Array.Sort(dados);
+                    Array.Reverse(dados);
+                    resultado = dados[0] + dados[1] + dados[2];
+                    atributos[x] = resultado;
+                } while (resultado < 8);          
+            }          
+                txtFue.Text = atributos[0].ToString();
+                txtCar.Text = atributos[1].ToString();
+                txtCon.Text = atributos[2].ToString();
+                txtDes.Text = atributos[3].ToString();
+                txtSab.Text = atributos[4].ToString();
+                txtInt.Text = atributos[5].ToString();
+            Bonificadores();
+        }
+
+         private void Bonificadores()
+        {
+            Int32.TryParse(txtFue.Text, out int Fue);
+            txtBonFue.Text = ((Fue - 10) / 2).ToString();
+        }   
     }
 }
